74b3d6cf91c28eefa81ee46e24931ed41f13f249
anand <anandbeemavarapu@gmail.com>
2024-05-28 20:33:58 +0530

    exception handler issue

--- empDemo/src/main/java/com/viz/empDemo/exception/GlobalExceptionHandler.java
+++ empDemo/src/main/java/com/viz/empDemo/exception/GlobalExceptionHandler.java
@@ -6,27 +6,28 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	
 	@ExceptionHandler(DataSourceException.class)
-	@ResponseStatus(HttpStatus.BAD_GATEWAY)
-	public ErrorResponse handleException(DataSourceException dse){
+	@ResponseStatus(value=HttpStatus.BAD_GATEWAY)
+	public ErrorResponse handleException(DataSourceException dse,WebRequest req){
 		return new ErrorResponse(dse.getMessage(), HttpStatus.BAD_GATEWAY.value());
 	}
 	
 	
 	@ExceptionHandler(EmployeeNotFoundException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ErrorResponse handleException(EmployeeNotFoundException nfe) {
+	@ResponseStatus(value=HttpStatus.NOT_FOUND)
+	public ErrorResponse handleException(EmployeeNotFoundException nfe,WebRequest req) {
 		return new ErrorResponse(nfe.getMessage(),HttpStatus.NOT_FOUND.value());
 	}
 	
 	@ExceptionHandler(EmpNotEmpty.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorResponse handleException(EmpNotEmpty en) {
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST)
+	public ErrorResponse handleException(EmpNotEmpty en,WebRequest req) {
 		return new ErrorResponse(en.getMessage(),HttpStatus.BAD_REQUEST.value());
 	}
 }
